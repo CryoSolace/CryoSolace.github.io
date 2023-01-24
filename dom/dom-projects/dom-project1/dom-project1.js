4277c9360f0fec364ce3c52ed93b920b4997a7e8
CryoSolace <100377930+CryoSolace@users.noreply.github.com>
2023-01-24 23:44:16 +0000

    Started DOM Project 1

---
+++ dom/dom-projects/dom-project1/dom-project1.js
@@ -0,0 +1,12 @@
+function generate() { // need to figure out how fetch apis work... good progress anyway.
+    fetch("https://api.quotable.io/random")
+    .then((data) => data.json())
+    .then((item) => {
+        document.querySelector(".quote").innerText = item.content;
+        document.querySelector(".author").innerText = item.author;
+    });
+};
+
+document.querySelector(".generate").addEventListener("click", generate);
+
+ 
